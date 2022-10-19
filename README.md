eb412946535b6d7930806530a47b82c4398bb0ef
Philipp Panhey <philipp.panhey@gmail.com>
2022-10-19 14:39:00 +0200

    added loc_nr and cnc_nr

--- README.md
+++ README.md
@@ -3,5 +3,13 @@ checks if there's an appointment (hb buergerservice) in a specific time.
  
  ## usage
  ```bash
- python3 hb_appointment_notification.py --startdate 25.07.2026 --enddate 30.07.2030 --telegramtoken TOKEN --telegramchatid CHAT_ID
+ python3 hb_appointment_notification.py --loc_nr {loc_nr} --cnc_nr { cnc_nr } --startdate 01.01.22 --enddate 31.12.30 --telegramtoken {telegramtoken} --telegramchatid {telegramchatid}
  ```
+
+ ## explanation specital params loc_nr and cnc_nr
+`loc_nr` is the location number e.g. 'BürgerServiceCenter-Mitte' results in loc_nr 535.
+`cnc_nr` is the service number e.g. Wohnsitz ummelden (Alleinige, Haupt- oder Nebenwohnung) results in cnc_nr 7329.
+
+Please go to https://termin.bremen.de/termine/ and click the location and service you're searching an appointment in order to find `loc_nr` and`cnc_nr`.
+
+
--- hb_appointment_notification.py
+++ hb_appointment_notification.py
@@ -8,7 +8,7 @@ def main():
     args = parse_args()
     cookie = extract_cookie()
     appointment_markup = get_appointment_markup(
-        cookie, args["startdate"], args["enddate"]
+        cookie, args["loc_nr"], args["cnc_nr"], args["startdate"], args["enddate"]
     )
     if appointment_available(appointment_markup):
         telegram_send_text(
@@ -20,6 +20,8 @@ def main():
 
 def parse_args():
     parser = argparse.ArgumentParser()
+    parser.add_argument("--loc_nr", required=True)
+    parser.add_argument("--cnc_nr", required=True)
     parser.add_argument("--startdate", required=True)
     parser.add_argument("--enddate", required=True)
     parser.add_argument("--telegramtoken", required=True)
@@ -34,8 +36,9 @@ def extract_cookie():
     return session.cookies.get_dict()["TVWebSession"]
 
 
-def get_appointment_markup(cookie, startdate, enddate):
-    url = f"https://termin.bremen.de/termine/suggest?loc=535&mdt=0&cnc-7022=1&filter_date_from={startdate}&filter_date_to={enddate}&suggest_filter=Filtern"
+def get_appointment_markup(cookie, loc_nr, cnc_nr, startdate, enddate):
+    url = f"https://termin.bremen.de/termine/suggest?loc={loc_nr}&mdt=0&cnc-{cnc_nr}=1&filter_date_from={startdate}&filter_date_to={enddate}&suggest_filter=Filtern"
+    print(url)
     headers = CaseInsensitiveDict()
     headers["Cookie"] = f"TVWebSession={cookie}"
     resp = requests.get(url, headers=headers)
